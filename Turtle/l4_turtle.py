0385e7109c1230e4abd9bf86a4648b347c24c87c
Abhay Singh Tomar <abhaysinghtomar97@gmail.com>
2025-10-23 19:34:32 +0530

    old

---
+++ Turtle/l4_turtle.py
@@ -0,0 +1,13 @@
+
+import turtle
+
+t= turtle
+
+t.forward(100)
+t.penup()
+t.forward(100)
+t.pendown()
+t.forward(100)
+t.penup()
+t.forward(100)
+t.pendown()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+turtle.forward(100)
